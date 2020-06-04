eb6c523eca0e681566031e59fc063b8cdac54c51
= <=>
2020-06-05 04:07:37 +0700

    generate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'statics/home'
+  get 'statics/home' , to: "statics#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home" , to: "statics#home"
   root "statics#home"
