f68fbd65e2857782d9a5fa3c400bf45131c59c99
= <=>
2020-06-05 04:21:16 +0700

    lib Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'statics/home' , to: "statics#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home" , to: "statics#home"
