29e89d217139b6ee26beb55ead6e533d57995c2d
= <=>
2020-06-05 03:57:53 +0700

    khai bao duong dan nhan vao

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'statics/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/home" , to: "statics#home"
+  root "statics#home"
 end
